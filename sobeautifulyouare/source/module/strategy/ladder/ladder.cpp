1e1b3083ddf5f8f7c583efeb6c54bb755102b3ef
Abraham <847575286@qq.com>
2016-07-13 11:18:03 +0800

    strategy for ladder

--- sobeautifulyouare/source/module/strategy/ladder/ladder.cpp
+++ sobeautifulyouare/source/module/strategy/ladder/ladder.cpp
@@ -3,32 +3,81 @@
 
 Ladder::Ladder()
 {
-	
+    double m_X_OFFSET = -15;
+    double m_FB_Step = 0;
+    int m_upcnt = 0;
+    int m_max_upcnt = 20;
+    int m_flatcnt = 0;
+    int m_flatcnttwo = 0;
+    int m_max_flatcnt = 10;
+    int m_downcnt = 0;
+    int m_max_downcnt = 20;
 }
 
 Ladder::~Ladder()
 {
 }
 
-
-
 void Ladder::ThreadMotion()
 {
     motion = new Motion();
     
-    
 	motion->poseInit();
 
-   
     while(true)
     {
 	    motion->CheckStatus();
-        
+
+        Walking::GetInstance()->start();
+        if(m_upcnt < m_max_upcnt)   //upstair
+        {
+            if(Walking::GetInstance()->X_OFFSET > -22)
+            {
+                Walking::GetInstance()->X_OFFSET -= 1;
+            }
+            motion->walk(22,0,0);
+            usleep(20 * 8000);
+            m_upcnt++;
+        }
+        else
+        {
+            if( m_flatcnt < m_max_flatcnt ) {//flat
+                if(Walking::GetInstance()->X_OFFSET < -10)
+                {
+                    Walking::GetInstance()->X_OFFSET += 1;
+                }
+                motion->walk(22,0,0);
+                usleep(20 * 8000);
+                m_flatcnt++;
+            }
+            else if(m_downcnt < m_max_downcnt)//downstair
+            {
+                if(Walking::GetInstance()->X_OFFSET < 0)
+                {
+                    Walking::GetInstance()->X_OFFSET += 1;
+                }
+                motion->walk(22,0,0);
+                usleep(20 * 8000);
+                m_downcnt++;
+            }
+            else//flat
+            {
+                if( m_flatcnttwo < m_max_flatcnt ) {//flat
+                    if(Walking::GetInstance()->X_OFFSET > -10)
+                    {
+                        Walking::GetInstance()->X_OFFSET -= 1;
+                    }
+                    motion->walk(22,0,0);
+                    usleep(20 * 8000);
+                    m_flatcnt++;
+                }
+            }
+        }
     }
 
 }
 
-int Ladder::GetImageResult()
+/*int Ladder::GetImageResult(cv::Mat &frame)
 {
     imgProc->imageProcess(frame,imgRes);
     ColorFindResult *tmp_result = dynamic_cast<ColorFindResult *>(imgRes);
@@ -38,5 +87,5 @@ int Ladder::GetImageResult()
     else {
 
     }
-}
+}*/
 
--- sobeautifulyouare/source/module/strategy/ladder/ladder.h
+++ sobeautifulyouare/source/module/strategy/ladder/ladder.h
@@ -3,6 +3,12 @@
 
 #include <strategy.h>
 
+enum LadderState{
+    UPSTAIR,
+    FLATWALKING,
+    DOWNSTAIR,
+};
+
 class Ladder:public Strategist
 {
 public:
@@ -10,11 +16,20 @@ public:
 	~Ladder();
 private:
 
+    double m_X_OFFSET ;
+    double m_FB_Step;
     int debug_print;
+    int m_upcnt;
+    int m_max_upcnt;
+    int m_flatcnt ;
+    int m_max_flatcnt ;
+    int m_downcnt ;
+    int m_max_downcnt ;
+    int m_flatcnttwo ;
 
 protected:
     virtual void ThreadMotion();
-    virtual int GetImageResult();
+    virtual int GetImageResult(cv::Mat &frame);
 };
 
 #endif // _LADDER_H_
