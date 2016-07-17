689c766504fe844b29dfa5d387ec764a9adb6bb7
Abraham <847575286@qq.com>
2016-07-17 20:50:34 +0800

    sprint beta 1.0

--- sobeautifulyouare/source/module/strategy/sprint/sprint.cpp
+++ sobeautifulyouare/source/module/strategy/sprint/sprint.cpp
@@ -12,7 +12,7 @@ Sprint::Sprint()
      m_point_found = false;
      m_GoalFBStep = 0;
      m_GoalRLTurn = 0;
-     m_FBStep = 30.0;
+     m_FBStep = 10.0;
      m_RLTurn = 0;
     debug_print = 1; 
 
@@ -23,9 +23,9 @@ Sprint::Sprint()
     m_RLTurn_straight = 0;
 
     m_Xoffset = -10;
-    m_target_Xoffset = -16;
+    m_target_Xoffset = -20;
 
-    m_percent_CenterDiff = 0.1;
+    m_percent_CenterDiff = 0.01;
     m_no_point_found = 0;
     m_NopointMaxCount = 5;
     m_pre_action = 0;
@@ -43,16 +43,17 @@ Sprint::~Sprint()
 void Sprint::ThreadMotion()
 {
     motion = new Motion();
-    tracker = new BallTracker();
-    follower = new BallFollower();
+    ColorFind *tmp_proc = dynamic_cast<ColorFind *>(imgProc);
+    tmp_proc->load("redbox.txt");
     //compasspro = new compass();
     //findline = new findline();
     
 	motion->poseInit();
-    Head::GetInstance()->MoveByAngle(0,20);
+    Head::GetInstance()->MoveByAngle(0, 50);
+    usleep(1000*8000);
     Walking::GetInstance()->Start();
     Walking::GetInstance()->X_OFFSET = m_Xoffset;
-    motion->walk(30,0,0);
+    motion->walk(m_FBStep,0,0);
     Walking::GetInstance()->Start();
     
     int tmp_img_result;
@@ -65,11 +66,18 @@ void Sprint::ThreadMotion()
         }
         tmp_img_result = GetImageResult();
 
-        if( m_Xoffset < m_target_Xoffset)
+        if( m_Xoffset > m_target_Xoffset)
         {
+		if ( debug_print ) fprintf(stderr,"oFFset %d %d\n", m_Xoffset, m_target_Xoffset);
             m_Xoffset -= 2;
         }
-        if(tmp_img_result = -1)
+	Walking::GetInstance()->X_OFFSET = m_Xoffset;
+        if(m_FBStep < m_FBStep_straight)
+        {
+            m_FBStep += 2;
+        }
+
+        if(m_point_found != true)
         {
             if ( debug_print ) fprintf(stderr,"Can't find the track!\n");
             m_no_point_found++;
@@ -80,21 +88,21 @@ void Sprint::ThreadMotion()
         }
         else
         {
-            if(m_pre_action = 0){
+            if(m_pre_action == 0){
                 SprintForward();
                 if ( debug_print ) fprintf(stderr,"I'm walking foward!\n");
             }
-            else if(m_pre_action = 1){
-                motion ->walk(m_FBStep_straight,0,m_RLTurn_left);   //haven't LostDispose but need turn easy left
-                if ( debug_print ) fprintf(stderr,"I'm walking easy left!\n");
+            else if(m_pre_action == 1){
+                motion ->walk(m_FBStep,0,m_RLTurn_left);   //haven't LostDispose but need turn easy left
+                if ( debug_print ) fprintf(stderr,"I'm walking easy left! %d\n",m_RLTurn_left);
             }
-            else if(m_pre_action = -1){
-                motion ->walk(m_FBStep_straight,0,m_RLTurn_right);   //haven't LostDispose but need turn easy right
+            else if(m_pre_action == -1){
+                motion ->walk(m_FBStep,0,m_RLTurn_right);   //haven't LostDispose but need turn easy right
                 if ( debug_print ) fprintf(stderr,"I'm walking easy right!\n");
             }
         }
 
-        m_percent_CenterDiff += 0.02;
+        m_percent_CenterDiff += 0.0002;
     }
 }
 
@@ -102,10 +110,9 @@ void Sprint::SprintForward()
 {
     if(debug_print) printf("SprintForward \n");
     if(  m_point_found == 1){//find point
-        m_RLTurn = m_RLTurn_straight;
-        m_FBStep = m_FBStep_straight;
+        motion->walk(m_FBStep , 0,m_RLTurn );
     }
-    motion->walk(m_FBStep , 0,m_RLTurn );
+
 }
 
 
@@ -115,23 +122,26 @@ int Sprint::GetImageResult()
     imgProc->imageProcess(frame,imgRes);
     ColorFindResult *tmp_result = dynamic_cast<ColorFindResult *>(imgRes);
     if ( tmp_result->valid == false ){
-        return -1;
+        return  -1;
     }
     else {
         m_point_found = true;
         m_point_center_2D.X = tmp_result->center.x;
         m_point_center_2D.Y = tmp_result->center.y;
-        m_RLturn_degree = fabs((m_point_center_2D.X - IMG_WIDTH / 2) / IMG_WIDTH);
-        if(m_point_center_2D.X >  IMG_WIDTH/2 &&  m_RLturn_degree > percent_CenterDiff) {
+        m_RLturn_degree = fabs((m_point_center_2D.X - 160.0) / 320.0);
+
+        if(debug_print) fprintf(stderr,"Center %lf IMG_Center 160 degree %lf now %lf\n",m_point_center_2D.X,m_RLturn_degree,m_percent_CenterDiff);
+        if(m_point_center_2D.X > 160.0&&  m_RLturn_degree > m_percent_CenterDiff ) {
             m_pre_action = -1;  //-1 point is on the right
         }
-        else if(m_point_center_2D.X < IMG_WIDTH/2 &&  m_RLturn_degree > percent_CenterDiff){
+        else if(m_point_center_2D.X < 160.0&&  m_RLturn_degree >m_percent_CenterDiff ){
             m_pre_action = 1;   //1 point is on the left
         }
         else
         {
-            m_pre_action = 0;d
+            m_pre_action = 0;
         }
+        return 0;
     }
 	is_new_img = false;
 }
@@ -139,19 +149,19 @@ int Sprint::GetImageResult()
 void Sprint::LostDispose()
 {
     if(debug_print) fprintf(stderr,"I should dispose the lost\n");
-    if(m_pre_action == -1)  //right
+    if(m_pre_action == -1)  //point is on the right
     {
-	if ( debug_print ) fprintf(stderr,"Lost right!\n");
-        motion->walk(5,8,-2);    //pinyi
+        if ( debug_print ) fprintf(stderr,"Lost right!\n");
+        motion->walk(5,8,0);    //pinyi right
     }
-    else if(m_pre_action == 1)  //left
+    else if(m_pre_action == 1)  //point is on the left
     {
 		if ( debug_print ) fprintf(stderr,"Lost left!\n");
-        motion->walk(5,-6,2);   //pinyi
+        motion->walk(5,-6,0);   //pinyi left
     }
 	else
 	{
-		motion->walk(30,0,0);    
+        motion->walk(m_FBStep,0,0);
         Walking::GetInstance()->Start();
 	}	
 }
